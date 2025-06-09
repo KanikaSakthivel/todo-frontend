85c9483fb02b07c34e27e47a3cf0a98c3de47f3b
KanikaSakthivel <kanikasakthivel15@gmail.com>
2025-06-09 14:41:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 export default function App() {
   console.log("✅ App component is rendering");
   return (
-    <div style={{ padding: "2rem" }}>
+    <div style={{ padding: "2rem", border: "2px solid red" }}>
       <h1>ToDo App Rendered!</h1>
     </div>
   );
